593e30b51eef3e9f1bf093e35496562f6f40f8c5
Dorian Raud <dorian.raud@gmail.com>
2026-02-15 15:34:38 +0100

    Fix : Pin zone intervention + logo ico

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang="fr">
             <Head>
                 <meta charSet="utf-8" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/final-logo.ico" />
             </Head>
             <body>
                 <Main />
