4dde141ea015a50373c37d609eec51a1e46ef8bf
Dorian Raud <dorian.raud@gmail.com>
2026-02-19 21:24:02 +0100

    Fix : commentaire de google analytic

--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,21 @@ import Footer from '../components/Footer';
 export default function App({ Component, pageProps }) {
     return (
         <>
-            {/* Google Analytics - Remplacez G-XXXXXXXXXX par votre ID de mesure */}
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-XXXXXXXXXX');
-                `}
-            </Script>
+            {/* Google Analytics - Remplacez G-XXXXXXXXXX par votre ID de mesure
+                <Script
+                    strategy="afterInteractive"
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-XXXXXXXXXX');
+                    `}
+                </Script>
+            */}
+
 
             <Navbar />
             <main style={{ marginTop: '80px' }}>
