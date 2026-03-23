da92fef4135fb0f432cdced2f9b0e5174ae1f3d1
Dorian Raud <dorian.raud@gmail.com>
2026-03-23 12:19:25 +0100

    Fix : suppression lien confidentialité

--- components/Footer.js
+++ components/Footer.js
@@ -80,7 +80,6 @@ export default function Footer({ initialTranslations }) {
                     <p>&copy; {currentYear} MaviPool. {t('footer.rights')}</p>
                     <div className={styles.legalLinks}>
                         <Link href="/mentions-legales">{t('footer.legal')}</Link>
-                        <Link href="#confidentialite">{t('footer.privacy')}</Link>
                     </div>
                 </div>
             </div>
